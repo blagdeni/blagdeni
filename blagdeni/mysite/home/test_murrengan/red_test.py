d72faf8fc46ef79ef610eb0927beb4503935cd40
blagdeni <49894100+blagdeni@users.noreply.github.com>
2019-04-26 21:17:40 +0300

    Opgrate mysite
    Opgrate mysite

--- blagdeni/mysite/home/test_murrengan/red_test.py
+++ blagdeni/mysite/home/test_murrengan/red_test.py
@@ -1,6 +1,9 @@
 import time
 import pytest
 from selenium import webdriver
+#from django.urls import resolve
+
+#from home.views import home
 
 @pytest.fixture
 def browser():
@@ -10,14 +13,20 @@ def browser():
 
 
 def test_title(browser):
-    time.sleep(1)
+    time.sleep(2)
 
     browser.get('http://127.0.0.1:8000/home/')
-    header_text = browser.find_element_by_tag_name('h1').text
-    inputbox = browser.find_element_by_id('Rrrr')
-    inputbox.send_keys('Blagdeni')
-    time.sleep(1)
+#    header_text = browser.find_element_by_tag_name('h1').text
+#    inputbox = browser.find_element_by_id('Rrrr')
+#    inputbox.send_keys('Blagdeni')
+    time.sleep(2)
 
-    assert header_text == 'Blagdeni'
+#    assert header_text == 'Blagdeni'
     assert browser.title == 'Blagdeni'
+
+#def test_home():
+#    found = resolve('/home/')
+#    assert found.func == home
+
+
 
